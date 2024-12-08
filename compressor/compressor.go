77d1f926abb13b4275b9da555411c3985c90ed4f
sumit-behera-in <sumitbehera1508@gmail.com>
2024-12-09 02:45:15 +0530

    compress according to type added

--- compressor/compressor.go
+++ compressor/compressor.go
@@ -2,20 +2,41 @@ package compressor
 
 import (
 	"log"
+	"path/filepath"
 
 	"github.com/h2non/bimg"
 )
 
+func findType(ext string) string {
+
+	switch ext {
+	case ".jpg", ".jpeg", ".png", ".webp", ".bmp":
+		return imageFileType
+	}
+
+	return ""
+}
+
 func Compress(file File) File {
-	return ImageCompress(file)
+	fileExtension := filepath.Ext(file.FileName)
+
+	fileType := findType(fileExtension)
+
+	if fileType == imageFileType {
+		return ImageCompress(file)
+	}
+
+	return file
 }
 
 func ImageCompress(inputFile File) File {
+
 	outputFile := File{}
 	outputFile.FileName = inputFile.FileName
 	var err error
 	outputFile.File, err = bimg.NewImage(inputFile.File).Process(bimg.Options{
-		Quality:       80,   // Set quality (e.g., 80)
+		Type:          bimg.WEBP,
+		Quality:       75,   // Set quality (e.g., 80)
 		StripMetadata: true, // Remove unnecessary metadata
 	})
 
---
+++ compressor/util.go
@@ -0,0 +1,5 @@
+package compressor
+
+const (
+	imageFileType = "img"
+)
